21c4778fb103590bac7ed7b597f505a72312dc4b
T-e-y-e <teyeetukudo@gmail.com>
2022-09-10 18:42:34 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,41 @@
 module.exports = {
   content: ['./dist/**/*.{html,js}'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      colors: {
+        'main': '#00B612',
+        'main-100': '#58DF66',
+        'main-200': '#19db2c',
+        'main3100': '#098D16',
+        'main-400': '#035916',
+        'main-500': '#002709',
+        'brand-dark': '#2D2D2D',
+        'brand-dark-100': '#3F3D3D',
+        'brand-gray': '##F2F3F5',
+        'brand-gray-100': '#DBDEE0',
+        'brand-gray-200': '#999A9C',
+        'alt': '#FFF5E5'
+      },
+      zIndex: {
+        'infinite': '99999',
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      },
+      boxShadow: {
+        'custom': '0 0 10px 0 rgba(26, 26, 26, 0.12)',
+      },
+    },
   },
   plugins: [],
 }
